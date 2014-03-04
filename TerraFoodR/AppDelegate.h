ed72a0b378d0234b2dbf2b4b31330963159336ae
Roman Rybachenko <rom_rybachenko@ukr.net>
2014-03-04 11:02:03 +0200

    Initial Commit

---
+++ TerraFoodR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TerraFoodR
+//
+//  Created by Roman Rybachenko on 3/4/14.
+//  Copyright (c) 2014 Roman Rybachenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TerraFoodRTests/TerraFoodRTests.m
@@ -0,0 +1,34 @@
+//
+//  TerraFoodRTests.m
+//  TerraFoodRTests
+//
+//  Created by Roman Rybachenko on 3/4/14.
+//  Copyright (c) 2014 Roman Rybachenko. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TerraFoodRTests : XCTestCase
+
+@end
+
+@implementation TerraFoodRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
